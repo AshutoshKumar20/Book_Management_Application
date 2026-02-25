6e7056b27f9cc61092d8fb425af9bd8b826d0b21
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-02-25 20:43:11 +0530

    Completed GET route for getting a single user by id

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { users } = require("./data/users.json")
+const { users } = require("./data/users.json");
 const app = express();
 
 const port = 8081;
@@ -26,6 +26,30 @@ app.get("/users", (req, res) => {
         data: users
     });
 });
+
+/* For get a user by id
+Route: /users/:id
+Method: GET
+Description: Get a single user by id
+Access: Public
+Parameters: id
+*/
+
+app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User Not Found"
+        });
+    } else {
+        return res.status(202).json({
+            success: true,
+            data: user
+        });
+    }
+})
 
 app.use((req, res) => {
     res.status(404).json({
