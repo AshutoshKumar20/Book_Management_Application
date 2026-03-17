494a608b1a99bd82cd9bdb58682b5ffaac5d56ed
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-03-17 15:19:13 +0530

    Got a single book by id using GET Route

--- routes/books.js
+++ routes/books.js
@@ -3,10 +3,43 @@ const { books } = require("../data/books.json");
 
 const router = express.Router();
 
+/* For getting all books 
+Route: /books
+Method: GET
+Description: Get all books
+Access: Public
+Parameters: none
+*/
+
 router.get("/", (req, res) => {
     res.status(202).json({
         success: true,
         data: books
+    })
+});
+
+/* For get a book by id
+Route: /books/:id
+Method: GET
+Description: Get a single book by id
+Access: Public
+Parameters: id
+*/
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    const book = books.find((each => each.id === id));
+    if (!book) {
+        return res.status(404).json({
+            success: false,
+            message: "Book Not Found"
+        })
+    }
+
+    return res.status(200).json({
+        success: true,
+        data: book
     })
 })
 
