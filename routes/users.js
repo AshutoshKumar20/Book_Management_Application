0a7b0e88f80905a3088feea41557e38bf4540176
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-04-09 14:28:39 +0530

    All routes are completed successfully

--- routes/users.js
+++ routes/users.js
@@ -152,19 +152,41 @@ router.get("/subscription-details/:id", (req, res) => {
         } else {
             date = new Date(data);
         }
-        let days = Math.floor(date / 1000 * 60 * 60 * 24);
+        let days = Math.floor(date / (1000 * 60 * 60 * 24));
         return days;
     }
-    const subscriptionType = (data) => {
+
+    const subscriptionType = (date) => {
         if (user.subscriptionType === "Basic") {
             date = date + 90;
         } else if (user.subscriptionType === "Standard") {
             date = date + 180;
-        } else if (user.subscriptionType === "Basic") {
+        } else if (user.subscriptionType === "Premium") {
             date = date + 365;
         }
+        return date;
     };
-})
+
+    let returnDate = getDateInDays(user.returnDate);
+    let currentDate = getDateInDays();
+    let subscriptionDate = getDateInDays(user.subscriptionDate);
+    let subscriptionExpiration = subscriptionType(subscriptionDate);
+
+    const data = {
+
+        ...user,
+        subscriptionExpired: subscriptionExpiration < currentDate,
+        daysLeftForExpiration:
+            subscriptionExpiration <= currentDate
+                ? 0 :
+                subscriptionExpiration - currentDate,
+        fine: returnDate < currentDate ? subscriptionExpiration <= currentDate ? 200 : 100 : 0,
+    };
+    res.status(200).json({
+        success: true,
+        data,
+    });
+});
 
 
 module.exports = router;
